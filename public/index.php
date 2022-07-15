7fba9e661cd20c8627dd20e63498bc4317da02b3
khanhhoang1997 <khanhlatom@gmail.com>
2022-07-15 14:27:26 +0700

    cc

--- public/index.php
+++ public/index.php
@@ -43,7 +43,8 @@ require __DIR__.'/../vendor/autoload.php';
 | to this client's browser, allowing them to enjoy our application.
 |
 */
-
+echo 'cc';
+die;
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
